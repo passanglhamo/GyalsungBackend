53eaf2529d735bb69c58bb2967bab516aee3436b
nzepa74 <nzepa.ttpl@gmail.com>
2022-10-24 18:09:32 +0600

    removed zongkhag and geog entity

--- user-profile/src/main/java/com/microservice/erp/domain/repositories/IDzongkhagRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.microservice.erp.domain.repositories;
-//
-//import com.microservice.erp.domain.entities.Dzongkhag;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface IDzongkhagRepository extends JpaRepository<Dzongkhag, String> {
-//    Dzongkhag findByDzongkhagId(Integer presentDzongkhagId);
-//}
