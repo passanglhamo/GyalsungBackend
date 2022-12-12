6cfdc372f7bc67aa5b9fcf48978038fd083a3bae
Passang Lhamo <passang.lhamo@thimphutechpark.bt>
2022-12-12 18:18:37 +0600

    Added docker compose file

--- auth-service/src/main/java/com/microservice/erp/AuthApplication.java
+++ auth-service/src/main/java/com/microservice/erp/AuthApplication.java
@@ -3,6 +3,8 @@ package com.microservice.erp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Rajib Kumer Ghosh
@@ -11,6 +13,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
+@ComponentScan(basePackages = {
+		"com.microservice.erp.controllers"
+		, "com.microservice.erp.services"
+		, "com.microservice.erp.domain"
+		, "com.microservice.erp.webapp.config"})
 public class AuthApplication {
 
 	public static void main(String[] args) {
